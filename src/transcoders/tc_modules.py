bff7c1f0bc264c8bb5ff00e5bec824bd058e9152
gboxo <gboxocor7@alumnes.ub.edu>
2024-09-08 02:53:56 +0200

    Finally tc attribution is working, thanks to the code from the Linear Circuits Paper Langugage-Model-SAEs repo

---
+++ src/transcoders/tc_modules.py
@@ -0,0 +1,253 @@
+from multiprocess import context
+from transformer_lens import HookedTransformer
+from transformer_lens.hook_points import HookPoint
+from typing import List, Tuple, Union, Callable, Dict, Any, Optional
+import torch
+from functools import partial
+from sae_training.sparse_autoencoder import SparseAutoencoder
+from contextlib import contextmanager
+from dataclasses import dataclass
+from abc import ABC
+
+from tc_utils import (
+    compose_hooks,
+    retain_grad_hook,
+    detach_hook,
+    mount_hooked_modules,
+    apply_sae,
+    Node,
+    Cache
+        )
+
+from transformer_lens.hook_points import NamesFilter
+
+def get_ref_caching_hooks(
+    model,
+    names_filter,
+    retain_grad: bool = False,
+    cache: Optional[dict] = None,
+) -> Tuple[dict, list]:
+    """Creates hooks to keep references to activations. Note: It does not add the hooks to the model.
+
+    Args:
+        names_filter (NamesFilter, optional): Which activations to cache. Can be a list of strings (hook names) or a filter function mapping hook names to booleans. Defaults to lambda name: True.
+        retain_grad (bool, optional): Whether to retain gradients for the activations. Defaults to False.
+        cache (Optional[dict], optional): The cache to store activations in, a new dict is created by default. Defaults to None.
+
+    Returns:
+        cache (dict): The cache where activations will be stored.
+        fwd_hooks (list): The forward hooks.
+    """
+    if cache is None:
+        cache = {}
+
+    if names_filter is None:
+        names_filter = lambda name: True
+    elif isinstance(names_filter, str):
+        filter_str = names_filter
+        names_filter = lambda name: name == filter_str
+    elif isinstance(names_filter, list):
+        filter_list = names_filter
+        names_filter = lambda name: name in filter_list
+    elif callable(names_filter):
+        names_filter = names_filter
+    else:
+        raise ValueError("names_filter must be a string, list of strings, or function")
+    assert callable(names_filter)  # Callable[[str], bool]
+
+    def save_hook(tensor: torch.Tensor, hook: HookPoint):
+        if retain_grad:
+            tensor.retain_grad()
+        cache[hook.name] = tensor
+
+    fwd_hooks = []
+    for name, hp in model.hook_dict.items():
+        if names_filter(name):
+            fwd_hooks.append((name, save_hook))
+
+    return cache, fwd_hooks
+
+
+def run_with_ref_cache(
+    model,
+    toks,
+    names_filter: NamesFilter = None,
+    retain_grad: bool = False,
+    reset_hooks_end=True,
+    clear_contexts=False,
+    
+):
+    """
+    Runs the model and returns the model output and a reference cache.
+
+    Args:
+        *model_args: Positional arguments for the model.
+        names_filter (NamesFilter, optional): A filter for which activations to cache. Accepts None, str,
+            list of str, or a function that takes a string and returns a bool. Defaults to None, which
+            means cache everything.
+        retain_grad (bool, optional): Whether to retain gradients for the activations. Defaults to False.
+        **model_kwargs: Keyword arguments for the model.
+
+    Returns:
+        tuple: A tuple containing the model output and the reference cache.
+
+    """
+
+    cache_dict, fwd = get_ref_caching_hooks(
+            model,
+        names_filter,
+        retain_grad=retain_grad,
+    )
+
+    with model.hooks(
+        fwd_hooks=fwd,
+        reset_hooks_end=reset_hooks_end,
+        clear_contexts=clear_contexts,
+    ):
+        model_out = model(toks)
+
+    return model_out, cache_dict
+
+
+class Attributor(ABC):
+    def __init__(
+        self,
+        model: HookedTransformer
+    ):
+        self.model = model
+
+    def attribute(
+        self,
+        input: Any,
+        target: Node,
+        candidates: list[Node],
+        **kwargs
+    ):
+        """
+        Attribute the target hook point of the model to given candidates in the model, w.r.t. the given input.
+
+        Args:
+            input (Any): The input to the model.
+            target: The target node to attribute.
+            candidates: The intermediate nodes to attribute to.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            nx.MultiDiGraph: The attributed graph, i.e. the circuit. Each node and edge should have an attribute "attribution",
+                showing its "importance" w.r.t. the target.
+        """
+        raise NotImplementedError
+
+    def cache_nodes(
+        self,
+        toks: Any,
+        nodes: list[Node],
+    ):
+        """
+        Cache the activation of  in the model forward pass.
+
+        Args:
+            toks (Any): The input to the model.
+            nodes (list[Node]): The nodes to cache.
+        """
+        output, cache = run_with_ref_cache(self.model,toks = toks, names_filter=[node.hook_point for node in nodes])
+        return Cache(output, cache)
+
+
+class HierachicalAttributor(Attributor):
+    def attribute(
+        self, 
+        toks: Any,
+        target: Node,
+        candidates: list[Node],
+        **kwargs
+    ) -> Dict:
+        """
+        Attribute the target node of the model to given candidates in the model, w.r.t. the given input.
+
+        Args:
+            toks (Any): The input to the model.
+            target: The target node to attribute.
+            candidates: The intermediate nodes to attribute to.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            nx.MultiDiGraph: The attributed graph, i.e. the circuit. Each node and edge should have an attribute "attribution",
+                showing its "importance" w.r.t. the target.
+        """
+
+        threshold: int = kwargs.get("threshold", 0.1)
+    
+        def generate_attribution_score_filter_hook():
+            v = None
+            def fwd_hook(tensor: torch.Tensor, hook: HookPoint):
+                nonlocal v
+                v = tensor
+                return tensor
+            def attribution_score_filter_hook(grad: torch.Tensor, hook: HookPoint):
+                assert v is not None, "fwd_hook must be called before attribution_score_filter_hook."
+                return (torch.where(v * grad > threshold, grad, torch.zeros_like(grad)),)
+            return fwd_hook, attribution_score_filter_hook
+        attribution_score_filter_hooks = {candidate: generate_attribution_score_filter_hook() for candidate in candidates}
+        fwd_hooks = [(candidate.hook_point, compose_hooks(attribution_score_filter_hooks[candidate][0], retain_grad_hook)) for candidate in candidates]
+        with self.model.hooks(
+            fwd_hooks=fwd_hooks,
+            bwd_hooks=[(candidate.hook_point, attribution_score_filter_hooks[candidate][1]) for candidate in candidates]
+        ):
+            cache = self.cache_nodes(toks, candidates + [target])
+            cache[target].backward()
+
+            # Construct the circuit
+            circuit = {"nodes": {}, "edges": []}
+            circuit["nodes"][target] = {"attribution": cache[target].sum().item(), "activation": cache[target].sum().item()}
+
+
+            for candidate in candidates:
+                grad = cache.grad(candidate)
+                if grad is None:
+                    continue
+                attributions = grad * cache[candidate]
+                if len(attributions.shape) == 0:
+                    if attributions > threshold:
+                        circuit["nodes"][candidate] = {"attribution": attributions.item(), "activation": cache[candidate].item()}
+                else:
+                    for index in (attributions > threshold).nonzero():
+                        index = tuple(index.tolist())
+                        node_name = candidate.append_reduction(*index)
+                        # Add node to circuit
+                        circuit["nodes"][node_name] = {"attribution": attributions[index].item(), "activation": cache[candidate][index].item()}
+                        # Add edge to circuit
+                        circuit["edges"].append((node_name, target, {"attribution": attributions[index].item()}))                        
+
+
+        return circuit
+
+
+
+
+
+
+if __name__ == "__main__":
+    model = HookedTransformer.from_pretrained("gpt2")
+
+    transcoder_template = "/media/workspace/gpt-2-small-transcoders/final_sparse_autoencoder_gpt2-small_blocks.{}.ln2.hook_normalized_24576"
+
+    tcs_dict = {}
+    for i in range(5):
+        tc = SparseAutoencoder.load_from_pretrained(f"{transcoder_template.format(i)}.pt").eval()
+        tcs_dict[tc.cfg.hook_point] = tc
+    saes = list(tcs_dict.values())
+
+    candidates = None
+    toks = "The"
+
+    with apply_sae(model, saes):
+        with model.hooks([(f"blocks.{i}.attn.hook_attn_scores", detach_hook) for i in range(12)]):
+            attributor = HierachicalAttributor(model = model)
+
+            target = Node("blocks.4.hook_mlp_out.sae.hook_hidden_post",reduction="0.1.6720")
+            if candidates is None:
+                candidates = [Node(f"{sae.cfg.out_hook_point}.sae.hook_hidden_post") for sae in saes[:-1]] + [Node(f"blocks.{i}.attn.hook_attn_scores") for i in range(12)]
+            circuit = attributor.attribute(toks=toks, target=target, candidates=candidates, threshold=0.1)
+            print(len(circuit["nodes"].keys()))
+            print(circuit)
---
+++ src/transcoders/tc_utils.py
@@ -0,0 +1,143 @@
+
+from transformer_lens import HookedTransformer
+from transformer_lens.hook_points import HookPoint
+from typing import List, Tuple, Union, Callable, Dict, Any
+import torch
+from functools import partial
+from sae_training.sparse_autoencoder import SparseAutoencoder
+from contextlib import contextmanager
+from dataclasses import dataclass
+from abc import ABC
+
+def compose_hooks(*hooks):
+    """
+    Compose multiple hooks into a single hook by executing them in order.
+    """
+    def composed_hook(tensor: torch.Tensor, hook: HookPoint):
+        for hook_fn in hooks:
+            tensor = hook_fn(tensor, hook)
+        return tensor
+    return composed_hook
+
+def retain_grad_hook(tensor: torch.Tensor, hook: HookPoint):
+    """
+    Retain the gradient of the tensor at the given hook point.
+    """
+    tensor.retain_grad()
+    return tensor
+
+def detach_hook(tensor: torch.Tensor, hook: HookPoint):
+    """
+    Detach the tensor at the given hook point.
+    """
+    return tensor.detach().requires_grad_(True)
+
+@contextmanager
+def mount_hooked_modules(
+    model: HookedTransformer,
+    hooked_modules: List[Tuple[str, str, SparseAutoencoder]]
+):
+    """
+    Mount the hooked modules to the model.
+    """
+    for name, module_name, sae in hooked_modules:
+        hook_point = model.mod_dict[name]
+        hook_point._modules[module_name] = sae
+
+    model.setup()
+    yield model
+
+
+
+
+@contextmanager
+def apply_sae(
+    model: HookedTransformer,
+    saes: list[SparseAutoencoder]
+):
+    """
+    Apply the sparse autoencoders to the model.
+    """
+    fwd_hooks: list[Tuple[Union[str, Callable], Callable]] = []
+    def get_fwd_hooks(sae: SparseAutoencoder) -> list[Tuple[Union[str, Callable], Callable]]:
+        x = None
+        def hook_in(tensor: torch.Tensor, hook: HookPoint):
+            nonlocal x
+            x = tensor
+            return tensor
+        def hook_out(tensor: torch.Tensor, hook: HookPoint):
+            nonlocal x
+            assert x is not None, "hook_in must be called before hook_out."
+            reconstructed,_,_,_,_,_, = sae.forward(x)
+            x = None
+
+            return reconstructed + (tensor - reconstructed).detach()
+        return [(sae.cfg.hook_point, hook_in), (sae.cfg.out_hook_point, hook_out)]
+    for sae in saes:
+        hooks = get_fwd_hooks(sae)
+        fwd_hooks.extend(hooks)
+    with mount_hooked_modules(model,[(sae.cfg.out_hook_point, "sae", sae) for sae in saes]):
+        with model.hooks(fwd_hooks):
+            yield model
+
+
+@dataclass(frozen=True)
+class Node:
+    """
+    A node in the circuit.
+    """
+
+    hook_point: str | None
+    """ The hook point of the node. None means the node is the output of the model. """
+    reduction: str | None = None
+    """ The reduction function to apply to the node. """
+
+    def reduce(self, tensor: torch.Tensor) -> torch.Tensor:
+        reductions = self.reduction.split(".") if self.reduction is not None else []
+        for reduction in reductions:
+            if reduction == "max":
+                tensor = tensor.max()
+            elif reduction == "mean":
+                tensor = tensor.mean()
+            elif reduction == "sum":
+                tensor = tensor.sum()
+            else:
+                try:
+                    index = int(reduction)
+                    tensor = tensor[index]
+                except ValueError:
+                    raise ValueError(f"Unknown reduction function: {reduction} in {self.reduction}.")
+        return tensor
+    
+    def append_reduction(self, *reduction: list[str | int]) -> "Node":
+        reduction: str = ".".join(map(str, reduction))
+        return Node(self.hook_point, f"{self.reduction}.{reduction}" if self.reduction is not None else reduction)
+    
+    def __hash__(self):
+        return hash((self.hook_point, self.reduction))
+    
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return self.hook_point == other.hook_point and self.reduction == other.reduction
+    
+    def __str__(self) -> str:
+        hook_point = self.hook_point if self.hook_point is not None else "output"
+        return f"{hook_point}.{self.reduction}" if self.reduction is not None else hook_point
+
+class Cache:
+    def __init__(self, output, cache: dict[str, torch.Tensor]):
+        self.cache = cache
+        self.output = output
+
+    def tensor(self, node: Node) -> torch.Tensor:
+        return node.reduce(self[node.hook_point])
+    
+    def grad(self, node: Node) -> torch.Tensor | None:
+        grad = self[node.hook_point].grad
+        return node.reduce(grad) if grad is not None else None
+    
+    def __getitem__(self, key: Node | str | None) -> torch.Tensor:
+        if isinstance(key, Node):
+            return self.tensor(key)
+        return self.cache[key] if key is not None else self.output
